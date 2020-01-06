f14b8fdf4403503d0b1ec9a120bc79c5f18a6b62
= <=>
2020-01-06 12:07:52 +0700

    Viết 3/4 phần của đăng nhập người dùng mua hàng còn phần thêm tài khoản của người dùng

--- MayLocNuoc/Content/Log/Log.js
+++ MayLocNuoc/Content/Log/Log.js
@@ -1,1 +1,144 @@
-﻿
+﻿//dang nhap cua log phan dang nhap cua ngươi mua hang
+$("#btnLog_DanhNhap").click(function () {
+    var taikhoan = $("#AccdanhNhap").val();
+    var matKhau = $("#passdanhNhap").val();
+
+    $.ajax({
+        type: 'Post',
+        url: '/DangNhap/LogJson',
+        dataType: 'Json',
+        data: { taikhoan: taikhoan, matkhau: matKhau },
+        success: function (e) {
+            if (e === "0") {
+                alert("Tài Khoản Không Chính Xác");
+            }
+            else if (e === "1") {
+                alert("Mật Khẩu Không chính Xác");
+            }
+            else if (e === "2") {
+                location.href = "Product/Index";
+            }
+            else if (e === "3") {
+                alert("Xin Chào Vui Lòng Đăng Nhập Tại Đây");
+                location.href = "/QuanTri/DN";
+            }
+            else { }
+        },
+        error: function (e) {
+            alert("Xin Lỗi Hệ Thống Đang Chẩn Bị Bảo Trì");
+        }
+
+    });
+});
+
+//phan lay lai mat khau 
+$("#BtnLayPass").click(function () {
+    var EmailLayPass = $("#EmailLayPass").val();
+    var AccdanhNhap_Email = $("#AccdanhNhap_Email").val();
+    $.ajax({
+        type: 'Post',
+        url: '/DangNhap/registGetPasswordrationJson',
+        dataType: 'Json',
+        data: { AccdanhNhap_Email: AccdanhNhap_Email, EmailLayPass: EmailLayPass },
+        success: function (e) {
+            if (e === "1") {
+                alert("Tài Khoản Không Chính Xác");
+            }
+            else if (e === "2") {
+                alert("Gmail Không chính Xác");
+            }
+            else if (e === "3") {
+                alert("Vui Lòng Kiểm Tra Gmail");
+                location.replace("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+            }
+            else {
+                alert("Xin Lỗi Hệ Thống Đang Chẩn Bị Bảo Trì");
+            }
+        },
+        error: function (e) {
+            alert("Xin Lỗi Hệ Thống Đang Chẩn Bị Bảo Trì");
+        }
+
+    });
+
+});
+//phan doi mat khau 
+
+
+$("#btnChangePassword").click(function () {
+    var taikhoanthaydoi = $("#taikhoanthaydoi").val();
+    var passOld = $("#passOld").val();
+    var passNew = $("#passNew").val();
+    var PassAgain = $("#PassAgain").val();
+    if (passNew === PassAgain) {
+        $.ajax({
+            type: 'Post',
+            url: '/DangNhap/changePasswordJson',
+            dataType: 'Json',
+            data: {
+                taikhoanthaydoi: taikhoanthaydoi, passOld: passOld, passNew: passNew
+            },
+            success: function (e) {
+                if (e === "1") {
+                    alert("Chỉ được Phép nhập a-z A-Z 0-9");
+                }
+                else if (e === "2") {
+                    alert("Có Lỗi Hệ Thống Đang Bảo trì ");
+
+                }
+                else if (e === "3") {
+                    alert("Tài Khoản Không Chính xác");
+                }
+                else if (e === "4") {
+                    alert("Mật Khẩu Không Chính xác");
+                }
+                else if (e === "5") {
+                    alert("Mật Khẩu Đã Thay đổi");
+                    location.href = "/DangNhap/Log";
+                }
+                else {}
+            },
+            error: function (e) {
+
+            }
+
+        });
+    }
+    else {
+        alert("Mật Khẩu Không Giống Nhau");
+    }
+
+})
+//phan dang ky tai khoan 
+$("#Btn_Registration").click(function () {
+    var AccRegistration = $("#AccRegistration").val();
+    var PassRegistration = $("#PassRegistration").val();
+    var PassAgainRegistration = $("#PassAgainRegistration").val();
+    var NameRegistration = $("#NameRegistration").val();
+    var numberPhoneRegistration = $("#numberPhoneRegistration").val();
+    var AdressRegistration = $("#AdressRegistration").val();
+    var EmailRegistration = $("#EmailRegistration").val();
+    if (PassRegistration === PassAgainRegistration) {
+        $.ajax({
+            type: 'Post',
+            url: '/DangNhap/RegistrationJson',
+            dataType: 'Json',
+            data: {
+                AccRegistration: AccRegistration, PassRegistration: PassRegistration,
+                NameRegistration: NameRegistration, numberPhoneRegistration: numberPhoneRegistration,
+                AdressRegistration: AdressRegistration, EmailRegistration: EmailRegistration
+            },
+            success: function (e) {
+            },
+            error: function (e) {
+
+            }
+
+        });
+    }
+    else {
+
+    }
+
+
+})
--- MayLocNuoc/Controllers/DangNhapController.cs
+++ MayLocNuoc/Controllers/DangNhapController.cs
@@ -98,23 +98,155 @@ namespace MayLocNuoc.Controllers
 
             return Json(avg);
         }
-        public JsonResult dangnhap1123(string a, string b)
+        public JsonResult LogJson(string taikhoan, string matkhau)
         {
-
+            /*
+             * -1 là có lỗi xảy ra
+             * 0 la tai khoan khong chinh xac
+             * 1 là mạt khẩu không chính xác
+             * đăng nhập thành công xác định là admin hay người dùng
+             * 2 là người dùng 
+             * 3 là loại khonmg cho đăng nhập trang này 
+             */
             string trave = "";
-            string avc = MD5.ToMD5(b.Trim());
-            int c = db.accs.Where(n => n.taikhoan == a && n.matkhau == avc).Count();
-            if (c == 1)
+            try
             {
-                trave = a;
-                save.nhap(trave);
+
+
+                int c = db.accs.Where(n => n.taikhoan == taikhoan).Count();
+                if (c == 1)
+                {
+                    string avc = MD5.ToMD5(matkhau.Trim());
+                    int m = db.accs.Where(n => n.matkhau == avc && n.taikhoan == taikhoan).Count();
+                    if (m == 1)
+                    {
+                        var k = db.accs.Where(n => n.taikhoan == taikhoan && n.matkhau == avc).FirstOrDefault();
+                        if (k.chucvu == 3)
+                        {
+                            //luu cookie
+                            save.taikhoan = taikhoan.Trim();
+                            HttpCookie ck = new HttpCookie("Máy Lọc Nước Sơn Trần");
+                            ck.Value = maHoa.mahoa(taikhoan + "1507" + MD5.ToMD5(matkhau.Trim()));
+                            ck.Expires = DateTime.Now.AddDays(15);
+                            Response.Cookies.Add(ck);
+                            //luu section 
+                            trave = "2";//trave roi chuyen trang
+                        }
+                        else
+                        {
+                            trave = "3";
+                        }
+                    }
+                    else
+                    {
+                        trave = "1";
+                    }
+                }
+                else
+                {
+                    trave = "0";
+                }
             }
-            else
+            catch (Exception)
             {
-                trave = c.ToString();
+
+                trave = "-1";
             }
             return Json(trave);
         }
+        public JsonResult registGetPasswordrationJson(string AccdanhNhap_Email, string EmailLayPass)
+        {
+            /*
+             * 1 la tai khoan khong chinh xac
+             * 2 la gmail khong chinh xac 
+             * 3 la thanh cong 
+             * 0 la he thong gap loi
+             */
+            string Trave = "";
+            try
+            {
+                if (db.accs.Where(n => n.taikhoan == AccdanhNhap_Email).Count() == 1)
+                {
+                    if (db.accs.Where(n => n.taikhoan == AccdanhNhap_Email && n.email == EmailLayPass).Count() == 1)
+                    {
+                        guiMail gm = new guiMail();
+                        Ran r = new Ran();
+                        if (gm.ToguiMail(EmailLayPass, r.songaunhien().ToString()) == true)
+                        {
+                            Trave = "3";
+                        }
+                        else
+                        {
+                            Trave = "0";
+                        }
+                    }
+                    else
+                    {
+                        Trave = "2";
+                    }
+                }
+                else
+                {
+                    Trave = "1";
+                }
+            }
+            catch (Exception)
+            {
 
+                Trave = "0";
+            }
+            return Json(Trave);
+        }
+        //changPass
+        public JsonResult changePasswordJson(string taikhoanthaydoi, string passOld, string passNew)
+        {
+            /*
+             * 1 trong co ky tu dac biet
+             * 2 hệ thống gặp lỗi 
+             * 3 tai khoan khong chinh xac
+             * 4 mat khau khong chinh xac
+             * 5 mat khau da duowc thay doi
+             */
+            string avg = "";
+            try
+            {
+                if (KyTu.kiemtra(taikhoanthaydoi) == true || KyTu.kiemtra(passOld) == true || KyTu.kiemtra(passNew) == true)
+                {
+                    avg = "1";
+                }
+                else
+                {
+                    if(db.accs.Where(n=>n.taikhoan==taikhoanthaydoi).Count()==1)
+                    {
+                       var mkOld= MD5.ToMD5(passOld.Trim());
+                        if (db.accs.Where(n => n.taikhoan == taikhoanthaydoi && n.matkhau==mkOld).Count() == 1)
+                        {
+                            var ChangPassss = db.accs.Where(n => n.taikhoan == taikhoanthaydoi && n.matkhau == mkOld).FirstOrDefault();
+
+                            ChangPassss.matkhau = MD5.ToMD5(passNew.Trim());
+
+                            db.SaveChanges();
+                            avg = "5";
+                        }
+                        else
+                        {
+                            avg = "4";
+                        }
+                    }
+                    else
+                    {
+                        avg = "3";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                avg = "2";
+            }
+            
+
+            return Json(avg);
+        }
     }
 }
