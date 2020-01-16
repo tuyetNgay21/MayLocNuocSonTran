81c8ced50bea048a88290c33dd72431c91ef20f6
= <=>
2020-01-16 09:16:14 +0700

    con cai gi khong

---
+++ MayLocNuoc/Content/NCC/all/jsThemSanPham/inDayMonth.js
@@ -0,0 +1,12 @@
+﻿$('#BTNthang').click(function () {
+    var BTNChonThangNam = $('#BTNChonThangNam').val();
+    var BTNChonThangThang = $('#BTNChonThangThang').val();
+    if (BTNChonThangNam == 'Năm' || BTNChonThangThang == 'Tháng') {
+        alert('Cần Lựa Chọn Tháng Năm');
+    }
+    else {
+
+        location.href = '/NhaCungCap/Product_In_Day_inMonth/?thang=' + BTNChonThangThang + '&nam=' + BTNChonThangNam+'';
+               
+    }
+});
