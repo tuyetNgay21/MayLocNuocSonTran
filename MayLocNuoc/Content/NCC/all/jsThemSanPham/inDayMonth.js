275959dc5bba6ac3a25f9187bac3bffec0bc80ce
= <=>
2020-01-16 12:06:49 +0700

    cap nhat

--- MayLocNuoc/Content/NCC/all/jsThemSanPham/inDayMonth.js
+++ MayLocNuoc/Content/NCC/all/jsThemSanPham/inDayMonth.js
@@ -10,3 +10,31 @@
                
     }
 });
+
+$('#btnMap2').click(function () {
+    var BTNChonThangNam = $('#btnnamMap2').val();
+    var BTNChonThangThang = $('#btnthangMap2').val();
+    var btnngayMap2 = $('#btnngayMap2').val();
+    if (BTNChonThangNam == 'Năm' || BTNChonThangThang == 'Tháng') {
+        alert('Cần Lựa Chọn Tháng Năm');
+    }
+    else {
+
+        location.href = '/NhaCungCap/Product_In_Day_inMonth/?thang=' + BTNChonThangThang + '&nam=' + BTNChonThangNam + '&ngay=' + btnngayMap2 + '';
+
+    }
+});
+
+$('#btnchitiet').click(function () {
+    var BTNChonThangNam = $('#btnchitietnam').val();
+    var BTNChonThangThang = $('#btnchitietThang').val();
+    var btnngayMap2 = $('#btnchitietngay').val();
+    if (BTNChonThangNam == 'Năm' || BTNChonThangThang == 'Tháng') {
+        alert('Cần Lựa Chọn Tháng Năm');
+    }
+    else {
+
+        location.href = '/NhaCungCap/Product_In_Day_inMonth/?thang=' + BTNChonThangThang + '&nam=' + BTNChonThangNam + '&ngay=' + btnngayMap2 + '';
+
+    }
+});
